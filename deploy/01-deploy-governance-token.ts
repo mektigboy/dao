1a3bdec284e1f40acc1d1b73b62dc926b7010079
mektigboy <mektigboy@protonmail.com>
2022-09-02 15:27:12 -0500

    Update deploy/01-deploy-governance-token.ts

--- deploy/01-deploy-governance-token.ts
+++ deploy/01-deploy-governance-token.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
 const deployGovernanceToken: DeployFunction = async (
   hre: HardhatRuntimeEnvironment
@@ -9,16 +10,31 @@ const deployGovernanceToken: DeployFunction = async (
   const { deploy, log } = deployments;
 
   log("Deploying GovernanceToken...");
-
   const governanceToken = await deploy("GovernanceToken", {
     args: [],
     from: deployer,
     log: true,
   });
-
   log(`01 - Deployed contract 'GovernanceToke' at: ${governanceToken.address}`);
+  await delegate(governanceToken.address, deployer);
+  log("01 - Delegated.");
 };
 
 export default deployGovernanceToken;
 
 deployGovernanceToken.tags = ["all", "governance-token"];
+
+const delegate = async (
+  governanceTokenAddress: string,
+  delegatedAccount: string
+) => {
+  const governanceToken = await ethers.getContractAt(
+    "GovernanceToken",
+    governanceTokenAddress
+  );
+  const txResponse = await governanceToken.delegate(delegatedAccount);
+  await txResponse.wait(1);
+  console.log(
+    `Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`
+  );
+};
