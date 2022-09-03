75e91c958c9d9db0a4d61c4cf35fcc7e411c4d6b
mektigboy <mektigboy@protonmail.com>
2022-09-03 15:36:46 -0500

    Update deploy/02-deploy-timelock.ts

--- deploy/02-deploy-timelock.ts
+++ deploy/02-deploy-timelock.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { ethers } from "hardhat";
+import { EXECUTORS, MINIMUM_DELAY, PROPOSERS } from "../hardhat-helper-config";
 
 const deployTimelock: DeployFunction = async (
   hre: HardhatRuntimeEnvironment
@@ -8,6 +9,14 @@ const deployTimelock: DeployFunction = async (
   const { getNamedAccounts, deployments, network } = hre;
   const { deployer } = await getNamedAccounts();
   const { deploy, log } = deployments;
+
+  log("Deploying Timelock...");
+  const timelock = await deploy("Timelock", {
+    args: [MINIMUM_DELAY, PROPOSERS, EXECUTORS],
+    from: deployer,
+    log: true,
+  });
+  log(`02 - Deployed contract 'Timelock' at: ${timelock.address}`);
 };
 
 export default deployTimelock;
