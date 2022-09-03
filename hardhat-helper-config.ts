720c801d6f293f9df5883513fda024aae4158f1f
mektigboy <mektigboy@protonmail.com>
2022-09-03 17:54:30 -0500

    Update hardhat-helper-config.ts

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -5,3 +5,5 @@ export const QUORUM_PERCENTAGE = 4; // 4 percent
 export const EXECUTORS: string[] = [];
 export const MINIMUM_DELAY = 3600;
 export const PROPOSERS: string[] = [];
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
