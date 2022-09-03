de2b31e6e3bbd726435c28f6b331d6b54c619955
mektigboy <mektigboy@protonmail.com>
2022-09-03 15:36:35 -0500

    Update hardhat-helper-config.ts

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -1,3 +1,7 @@
 export const VOTING_DELAY = 1; // 1 block
 export const VOTING_PERIOD = 45818; // 1 week
 export const QUORUM_PERCENTAGE = 4; // 4 percent
+
+export const EXECUTORS: string[] = [];
+export const MINIMUM_DELAY = 3600;
+export const PROPOSERS: string[] = [];
