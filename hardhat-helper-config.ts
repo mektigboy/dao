266255ef4636ade383d4d4c18748740c7e53a2de
mektigboy <mektigboy@protonmail.com>
2022-09-03 22:50:54 -0500

    Update hardhat-helper-config.ts

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -1,9 +1,17 @@
-export const VOTING_DELAY = 1; // 1 block
-export const VOTING_PERIOD = 45818; // 1 week
 export const QUORUM_PERCENTAGE = 4; // 4 percent
+export const VOTING_DELAY = 1; // 1 block
+export const VOTING_PERIOD = 5; // 5 blocks
 
 export const EXECUTORS: string[] = [];
 export const MINIMUM_DELAY = 3600;
 export const PROPOSERS: string[] = [];
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+
+export const DESCRIPTION = "Proposal #1 - Update Box's value to 100";
+export const FUNCTION = "store";
+export const FUNCTION_ARGS = 100;
+
+export const developmentChains = ["hardhat", "localhost"];
+
+export const PROPOSAL_FILE = "proposal.json";
